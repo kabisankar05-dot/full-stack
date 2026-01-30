7f8addd2a44bc3cc8b3238d3fc58b5c19044f201
kabisankar05-dot <kabisankar05@gmail.com>
2026-01-30 15:44:40 +0530

    update json

--- script/json.js
+++ script/json.js
@@ -3,9 +3,9 @@
 
 /* ===== DATA STORE (IMPORTANT) ===== */
 let users = [
-  { name: "Kabi", amount: 5000, city: "Chennai", number: "9876543210" },
-  { name: "Arun", amount: 3000, city: "Madurai", number: "9123456789" },
-  { name: "Priya", amount: 7000, city: "Coimbatore", number: "9988776655" }
+  { id:"02", name: "yathav", email: "yathav12@gmail.com", amount: 5000, city: "Chennai", number: "9876543210", gender:"Male" },
+  { id:"01", name: "Arun", email: "arun12@gmail.com", amount: 3000, city: "Madurai", number: "9123456789", gender:"Male" },
+  { id:"03", name: "Priya", email: "priya12@gmail.com" ,amount: 7000, city: "Coimbatore", number: "9988776655", gender:"Female" }
 ];
 let editIndex = -1;
 
@@ -16,10 +16,13 @@ function display(data = users) {
   data.forEach((u, i) => {
     rows += `
       <tr>
+        <td>${u.id}</td>
         <td>${u.name}</td>
+        <td>${u.email}</td>
         <td>${u.amount}</td>
         <td>${u.city}</td>
         <td>${u.number}</td>
+        <td>${u.gender}</td>
         <td><button onclick="editUser(${i})">Edit</button></td>
         <td><button onclick="deleteUser(${i})">Delete</button></td>
       </tr>
@@ -31,21 +34,36 @@ function display(data = users) {
 
 /* ===== ADD / UPDATE ===== */
 function addUser() {
+  let id = document.getElementById("id").value.trim();
   let name = document.getElementById("name").value.trim();
+  let email = document.getElementById("email").value.trim();
   let amount = document.getElementById("amount").value.trim();
   let city = document.getElementById("city").value.trim();
   let number= document.getElementById("number").value.trim();
 
+  let gender =document.querySelector('input[name="gender"]:checked');
+
   let valid = true;
 
   // clear old errors
+  document.getElementById("idErr").innerHTML = "";
   document.getElementById("nameErr").innerHTML = "";
+  document.getElementById("emailErr").innerHTML = "";
   document.getElementById("amountErr").innerHTML = "";
   document.getElementById("cityErr").innerHTML = "";
   document.getElementById("numberErr").innerHTML = "";
+  document.getElementById("genderErr").innerHTML = "";
   
 
-  //validation
+       /* ==== validation ==== */
+  
+  //id validation
+  if (id === ""){
+    document.getElementById("idErr").innerHTML = "Enter your ID";
+    valid = false;
+  }
+  
+  //name validation
   if (name === "") {
     document.getElementById("nameErr").innerHTML = "Enter your name";
     valid = false;
@@ -54,17 +72,40 @@ function addUser() {
     valid = false;
   }
 
+  //email validation 
+  if (email === "") {
+    document.getElementById("emailErr").innerHTML ="Enter your Email";
+    valid = false;
+  }else if(!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)){
+    document.getElementById("emailErr").innerHTML = "Enter valid Email";
+    valid = false;
+  }
+
+
+  //amount validation
   if (amount === "" || isNaN(amount)) {
     document.getElementById("amountErr").innerHTML = "Enter valid amount";
     valid = false;
   }
 
-  if (city === "") {
-    document.getElementById("cityErr").innerHTML = "Enter city";
+  //gender validation
+  if(!gender) {
+    document.getElementById("genderErr").innerHTML = "Select gender";
     valid = false;
   }
 
-  if (!/^[0-9]{10}$/.test(number)) {
+  //city validation
+  if (city === "") {
+    document.getElementById("cityErr").innerHTML = "Enter your city";
+    valid = false;
+  }
+
+  //phone number validation
+  if (number === ""){
+    document.getElementById("numberErr").innerHTML= "Enter your phone number";
+    valid = false;
+  }
+  else if(!/^[0-9]{10}$/.test(number)) {
     document.getElementById("numberErr").innerHTML = "Enter valid 10 digit number";
     valid = false;
   }
@@ -72,10 +113,13 @@ function addUser() {
   if (!valid) return;
 
   let userObj = {
+    id,
     name,
+    email,
     amount: Number(amount),
     city,
-    number
+    number,
+    gender:gender.value
   };
 
   if (editIndex >= 0) {
@@ -91,12 +135,22 @@ function addUser() {
 
 /* ===== EDIT ===== */
 function editUser(index) {
+  document.getElementById("id").value = users[index].id;
   document.getElementById("name").value = users[index].name;
+  document.getElementById("email").value = users[index].email;
   document.getElementById("amount").value = users[index].amount;
   document.getElementById("city").value = users[index].city;
   document.getElementById("number").value = users[index].number;
+  document.getElementById("gender").value = users[index].gender;
+
+  document.querySelector(
+    `input[name="gender"][value="${users[index].gender}"]`
+  ).checked = true;
+
+
   editIndex = index;
 }
+
 
 /* ===== DELETE ===== */
 function deleteUser(index) {
@@ -131,8 +185,12 @@ function sortByAmount() {
 
 /* ===== CLEAR INPUTS ===== */
 function clearInputs() {
+  document.getElementById("id").value = "";
   document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
   document.getElementById("amount").value = "";
-  document.getElementById("city").value ="";
-  document.getElementById("number").value ="";
+  document.getElementById("city").value = "";
+  document.getElementById("number").value = "";
+  
+  document.querySelectorAll('input[name="gender"]').forEach(r =>r.checked = false);
 }
