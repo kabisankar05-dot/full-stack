f4e910fb2d7b2a57579a94183b94bb526a855b49
kabisankar05-dot <kabisankar05@gmail.com>
2026-03-14 14:50:20 +0530

    useform

--- react/my-first-app/src/RegistrationForm.jsx
+++ react/my-first-app/src/RegistrationForm.jsx
@@ -24,17 +24,21 @@ function RegistrationForm() {
           <h2>Registration Form</h2>
 
           {/* Full Name */}
+
           <label>Full Name</label>
           <input
             type="text"
+            placeholder='Enter Your Name'
             {...register("fullName", { required: "Full Name is required" })}
           />
           {errors.fullName && <p className="error">{errors.fullName.message}</p>}
         
           {/* Email */}
-          <label>Email</label>
+
+          <label>Email Address</label>
           <input
             type="email"
+              placeholder='Enter Your Email'
             {...register("email", {
               required: "Email is required",
               pattern: {
@@ -44,9 +48,89 @@ function RegistrationForm() {
             })}
           />
           {errors.email && <p className="error">{errors.email.message}</p>}
-        </form>
+
+            <div className="row">
+
+              {/* mobile number */}
+
+              <div className="col">
+                <label>Mobile Number</label>
+                <input type="number" 
+                  placeholder='Enter Your Mobile Number'
+                {...register("mobile number", { required: "Mobile Number is required" })} />
+                {errors.mobile && <p className="error">{errors.mobile.message}</p>}
+              </div>
+
+              <div className="col">
+
+                {/* Birth Date */}
+
+                <label>Birth Date</label>
+                <input type="date" 
+                {...register("birthDate", { required: "Birth Date is required" })} />
+                {errors.birthDate && <p className="error">{errors.birthDate.message}</p>}
+              </div>
+            </div>
+
+
+            {/* Gender */}
+          <label>Gender</label>
+          <div className="gender">
+
+            <label>
+              <input
+                type="radio"
+                value="Male"
+                {...register("gender",{required:"Select gender"})}
+              /> Male
+            </label>
+
+            <label>
+              <input
+                type="radio"
+                value="Female"
+                {...register("gender")}
+              /> Female
+            </label>
+
+            <label>
+              <input
+                type="radio"
+                value="Other"
+                {...register("gender")}
+              /> Prefer not to say
+            </label>
+
+          </div>
+          <p className="error">{errors.gender?.message}</p>
+
+
+              {/*country */}
+
+              <div className="row">
+                <div className="col">                
+                <select {...register("country", { required: "Select Country" })}>
+                  <option value="">Select Country</option>
+                  <option value="usa">USA</option>
+                  <option value="india">India</option>
+                  <option value="uk">UK</option>
+                </select>
+                {errors.country && <p className="error">{errors.country.message}</p>}
+              </div>
+              
+              <div className="col">
+
+                {/* postal code */}
+
+                <input type="text" placeholder='Enter Your Postal Code'
+                {...register("postalCode", { required: "Postal Code is required" })} />
+                {errors.postalCode && <p className="error">{errors.postalCode.message}</p>}
+                </div>
+              </div>
+              <button type="submit">Submit</button>
+          </form>
+        </div>
       </div>
-    </div>
   )
 }
 
--- react/my-first-app/src/Usereducer.jsx
+++ react/my-first-app/src/Usereducer.jsx
@@ -1,1 +1,25 @@
+    import React from 'react'
+    
+    function Usereducer() {
+       const userdata ={
+        name:"kabi",
+        age: 22,
+        phone: 1234567890,
+        city: "gobi"
+       }
+
+       let {name, age, phone, city} = userdata;
+     
+     
+        return (
+        <div>
+            <h1>Name: {name}</h1>
+            <h2>Age: {age}</h2>
+            <p>Phone: {phone}</p>
+            <p>City: {city}</p> 
+        </div>
+      )
+    }
+    
+    export default Usereducer
     
